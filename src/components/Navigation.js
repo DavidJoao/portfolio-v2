03f17676ac6f1f6d62c48a6dd093de20047e2026
DavidJoao <davidsandoval596@gmail.com>
2022-08-16 20:33:18 -0700

    added cards for projects, created component for the cards

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ function Navigation(){
           <Navbar.Brand href="#home">David Sandoval</Navbar.Brand>
           <Nav style={{float:'right'}} className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#projects">Work</Nav.Link>
             <Nav.Link href="#education">Education</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,12 @@
+import ProjectCard from "./ProjectCard"
+
 function Projects(){
     return(
-        <div id="projects" style={{height:'95vh', width:'100%', backgroundColor:'#E6EDF3'}}>Projects</div>
+        <div id="projects" style={{height:'95vh', width:'100%', backgroundColor:'#E6EDF3'}} className="projects-container">
+            <ProjectCard projectTitle={"Meet-N-Eat"} projectName={"Meet-N-Eat"}/>
+            <ProjectCard projectTitle={"MyWeather/Forecast Provider"} projectName={"MyWeather App"} />
+            <ProjectCard projectTitle={"Rick and Morty Card Explorer"} projectName={"Rick and Morty Explorer"}/>
+        </div>
     )
 }
 
