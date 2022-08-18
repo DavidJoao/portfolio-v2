b23e778c4dd43ae6c9a90d0ddaf8005f7256fc01
DavidJoao <davidsandoval596@gmail.com>
2022-08-18 10:58:43 -0700

    fixed photo bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home(){
     return(
         <div id='home' style={{backgroundColor:'#213642', width:'100%', height:'90vh', paddingTop:'5%'}}>
                 <Card className='main-card' style={{ width: '18rem', padding:'40px', margin:'0 auto', width:'80%', background:'none', border:'none', color:'white'}}>
-                    <Image style={{borderRadius:'100%', width:'250px', border:'5px solid #E6EDF3', boxShadow:"1px -1px 32px -5px #E6EDF3"}} src='https://scontent-lax3-2.xx.fbcdn.net/v/t39.30808-6/273769724_4795165370551322_2418480384812829365_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=UwulFLo-QFsAX-ozIdG&_nc_ht=scontent-lax3-2.xx&oh=00_AT8Ke8L2w1bs7pGrSIrjtcnSn4o8tg-GwUZSdZsOC9HFag&oe=62FD1393'/>
+                    <Image style={{borderRadius:'100%', width:'250px', border:'5px solid #E6EDF3', boxShadow:"1px -1px 32px -5px #E6EDF3"}} src='https://i.ibb.co/7jCBG8X/myphoto.jpg'/>
                     <Card.Body>
                         <Card.Title>David Sandoval</Card.Title>
                         <Card.Text>
