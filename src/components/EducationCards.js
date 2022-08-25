97f241190bba1cba3de385fe7e616a9442942e2c
DavidJoao <davidsandoval596@gmail.com>
2022-08-25 11:01:02 -0700

    changed education cards size

--- src/components/EducationCards.js
+++ src/components/EducationCards.js
@@ -2,8 +2,8 @@ import { Card, Button } from 'react-bootstrap'
 
 function EducationCards( {institutionName, date, logo, description, location} ){
     return(
-    <Card style={{ width: '18rem', marginBottom:'2%', border:'5px solid #E6EDF3', boxShadow:'0px 1px 17px 0px rgba(255,255,255,0.75)', height:'35rem'}} className="m-3 mt-4">
-        <Card.Img variant="top" src={logo} />
+    <Card style={{ width: '22rem', marginBottom:'2%', border:'5px solid #E6EDF3', boxShadow:'0px 1px 17px 0px rgba(255,255,255,0.75)', height:'35rem'}} className="m-3 mt-4">
+        <Card.Img variant="top" src={logo} style={{width:'70%', alignSelf:'center'}} />
         <Card.Body>
           <Card.Title>{institutionName}</Card.Title>
           <Card.Text>{date}<span style={{float:'right'}}>{location}</span></Card.Text>
